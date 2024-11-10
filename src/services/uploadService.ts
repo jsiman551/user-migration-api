127b345897161ce6129e5ca0c7d06495560b68cc
Jose Simancas <josesimancas51@gmail.com>
2024-11-10 10:54:53 -0500

    Fix: add user data to error response from upload

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -48,12 +48,15 @@ export const processCsvUpload = async (fileBuffer: Buffer): Promise<CsvUploadRes
                         if (error instanceof ZodError) {
                             errors.push({
                                 row: csvResults.length + 1,
+                                name,
+                                email,
+                                age,
                                 title: error.errors.map((e) => e.message).join(', '),
                                 source: { pointer: error.errors[0].path[0] },
                             });
                         } else if (error instanceof Error) {
                             errors.push({
-                                row: csvResults.length + 1,
+                                //row: csvResults.length + 1,
                                 title: error.message,
                                 details: error.name === 'SequelizeUniqueConstraintError' ? `${email} is already registered` : 'Error creating user',
                             });
